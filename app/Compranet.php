4fa019098d4c4fe70125e00c8394040cdde6bdf7
Julio Guerrero <juliocaramelo44@gmail.com>
2022-04-28 12:37:50 -0500

    agregado el modelo compranet

---
+++ app/Compranet.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Compranet extends Model
+{
+    use HasFactory;
+
+    protected $table = 'compranet';
+    protected $guarded = [];
+}
