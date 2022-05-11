3cabe885f38e7938ae713e58fd5903bc2cccd828
Julio Guerrero <juliocaramelo44@gmail.com>
2022-05-11 14:21:28 -0500

    creados los modelos para realizar los find

---
+++ app/ArticuloContrato.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ArticuloContrato extends Model
+{
+    use HasFactory;
+
+    protected $table = 'abastos';
+    protected $fillable = [];
+}
---
+++ app/DivContratos.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DivContratos extends Model
+{
+    use HasFactory;
+}
