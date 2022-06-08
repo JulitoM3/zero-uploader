bae077a88d416e3487d3ee68798afc766aafa2eb
Julio Guerrero <juliocaramelo44@gmail.com>
2022-06-08 13:06:46 -0500

    creado el modelo de altas_sai_prei

---
+++ app/AltaSaiPrei.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AltaSaiPrei extends Model
+{
+    use HasFactory;
+
+    protected $table = 'altas_sai_prei';
+}
+
